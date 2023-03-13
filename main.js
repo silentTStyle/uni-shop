5a5d0ecfaa0a6f791daa24b344b3a93d0417d1ae
silentTStyle <xiangfangchen@icloud.com>
2023-03-13 16:02:37 +0800

    完成首页功能开发

--- main.js
+++ main.js
@@ -3,6 +3,35 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求根路径
+$http.baseUrl = 'http://api-ugo-web.itheima.net'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装弹窗
+uni.$showMsg = function() {
+  uni.showToast({
+    title: '数据请求失败',
+    duration: 1500,
+    icon: 'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
